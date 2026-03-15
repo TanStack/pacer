160b4ff351c9242f54431903339866d3fffc080b
Harry Whorlow <79278353+harry-whorlow@users.noreply.github.com>
2026-03-16 00:11:56 +0100

    fix(devtools): bump packages and move theme provider (#176)

---
+++ .changeset/tasty-impalas-mate.md
@@ -0,0 +1,8 @@
+---
+'@tanstack/preact-pacer-devtools': patch
+'@tanstack/react-pacer-devtools': patch
+'@tanstack/solid-pacer-devtools': patch
+'@tanstack/pacer-devtools': patch
+---
+
+Extract devtools theme provider to respective devtools to prevent mismatch.
--- packages/pacer-devtools/src/PacerDevtools.tsx
+++ packages/pacer-devtools/src/PacerDevtools.tsx
@@ -1,10 +1,19 @@
+import { ThemeContextProvider } from '@tanstack/devtools-ui'
 import { PacerContextProvider } from './PacerContextProvider'
 import { Shell } from './components/Shell'
 
-export default function PacerDevtools() {
+import type { TanStackDevtoolsTheme } from '@tanstack/devtools-ui'
+
+interface PacerDevtools {
+  theme: TanStackDevtoolsTheme
+}
+
+export default function PacerDevtools(props: PacerDevtools) {
   return (
-    <PacerContextProvider>
-      <Shell />
-    </PacerContextProvider>
+    <ThemeContextProvider theme={props.theme}>
+      <PacerContextProvider>
+        <Shell />
+      </PacerContextProvider>
+    </ThemeContextProvider>
   )
 }
